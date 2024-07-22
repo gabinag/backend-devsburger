5f97a5706b34ba047644b93ac21f093a6aa3176a
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-07-22 10:29:00 -0300

    update listorders

--- src/controllers/ListOrdersController.ts
+++ src/controllers/ListOrdersController.ts
@@ -1,9 +1,13 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { ListOrdersService } from "../services/ListOrdersService";
 
+interface QueryParams {
+  status?: string;
+}
+
 class ListOrdersController {
-  async handle(request: FastifyRequest, reply: FastifyReply) {
-    const { status } = request.params as { status: string };
+  async handle(request: FastifyRequest<{ Querystring: QueryParams }>, reply: FastifyReply) {
+    const { status } = request.query;
     const listOrdersService = new ListOrdersService();
     const orders = await listOrdersService.execute(status);
     reply.send(orders);
