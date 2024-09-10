8ddbcae3d91f016c68f825af79b7bbd2429f468c
gabinag <gabriela.nagamuta@gmail.com>
2024-09-10 10:51:15 -0300

    update totalPrice

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -20,7 +20,7 @@ interface DeleteOrderProps {
 
 export class CreateOrderService {
   async execute({ name, phone, address, paymentMethod, items, status, observation, totalPrice }: CreateOrderProps) {
-    if (!name || !phone || !address || !paymentMethod || items.length === 0 || !totalPrice) {
+    if (!name || !phone || !address || !paymentMethod || items.length === 0 || !totalPrice || totalPrice <= 0) {
       throw new Error("Preencha todos os campos e adicione pelo menos um item ao pedido");
     }
 
@@ -32,7 +32,7 @@ export class CreateOrderService {
         paymentMethod,
         status,
         observation,
-        totalPrice,
+        totalPrice: Number(totalPrice),
         orderItems: {
           create: items.map(item => ({
             productId: item.productId,
